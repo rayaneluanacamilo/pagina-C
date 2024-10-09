fe77fa25dbba1f0915e52e16df1564f5e2668192
rayaneluanacamilo <108135357+rayaneluanacamilo@users.noreply.github.com>
2024-10-09 10:27:52 -0300

    02/10

---
+++ script.js
@@ -0,0 +1,9 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativa");
+const caixaResultados= document.querySelector("caixa-resultado");
+const texto-caixaResultados= document.querySelector("texto-resultado");
+
+const perguntas ={
+    encliado:
+}
